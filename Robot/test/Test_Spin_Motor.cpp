f475873e848e0dd7da7a2308244aee2a0378fb40
Arthur Neves <arthurneves@pop-os.localdomain>
2023-02-13 07:10:45 -0300

    Limpando codigo que n usaremos

--- Robot/test/Test_Spin_Motor.cpp
+++ Robot/test/Test_Spin_Motor.cpp
@@ -21,42 +21,6 @@ void setup()
 
 void loop()
 {
-/*
-//Gira o Motor A no sentido horario
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN1, Power);
-    digitalWrite(IN2, LOW);
-    delay(2000);
-//Para o motor A
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, HIGH);
-    delay(500);
-//Gira o Motor B no sentido horario
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, LOW);
-    delay(2000);
-//Para o motor B
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, HIGH);
-    delay(500);
-
-//Gira o Motor A no sentido anti-horario
-    digitalWrite(IN1, LOW);
-    digitalWrite(IN2, HIGH);
-    delay(2000);
-//Para o motor A
-    digitalWrite(IN1, HIGH);
-    digitalWrite(IN2, HIGH);
-    delay(500);
-//Gira o Motor B no sentido anti-horario
-    digitalWrite(IN3, LOW);
-    digitalWrite(IN4, HIGH);
-    delay(2000);
-//Para o motor B
-    digitalWrite(IN3, HIGH);
-    digitalWrite(IN4, HIGH);
-    delay(500);
-*/
 //girar os 2 juntos no sentido horario (anda pra frente)
     digitalWrite(IN1, HIGH);
     //digitalWrite(IN1, Power);
